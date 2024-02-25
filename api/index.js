45bd8d2fa4131060c1241b92e03321cf8518fbb0
Ritik <bajpayeeritik2403@gmail.com>
2024-02-25 20:13:56 +0530

    Connected to DB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from "express";
-
+import mongoose from "mongoose";
 const app = express();
+import dotenv from "dotenv";
+import { error } from "console";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected"))
+  .catch((error) => console.log(error));
 
 app.listen(3000, () => console.log("Server is running on 3000!!!"));
