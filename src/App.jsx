b46bc19a4e392054590d20d089f272fff8cefbb5
Ritik <bajpayeeritik2403@gmail.com>
2024-02-25 17:22:19 +0530

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
+import Header from "./Components/Header";
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/sign-in" element={<SignIn />}></Route>
-      <Route path="/sign-up" element={<SignUp />}></Route>
-      <Route path="/about" element={<About />}></Route>
-      <Route path="/profile" element={<Profile />}></Route>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 }
